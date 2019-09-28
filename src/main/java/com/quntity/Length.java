d231ae899f15c7d2b65a89779dd502758c753964
sunilsyenare <syenare97@gmail.com>
2019-09-28 11:36:24 +0530

    [sunil|satya] Refactor. Remove inch class and add all functionality of it to length

--- src/main/java/com/quntity/Length.java
+++ src/main/java/com/quntity/Length.java
@@ -1,22 +1,23 @@
 package com.quntity;
 
 public class Length {
-    public enum Unit{
-        FEET,INCH
+    public enum Unit {
+        FEET, INCH
     }
 
     private final float value;
     private final Unit unit;
 
-    public Length(float value,Unit unit) {
+    public Length(float value, Unit unit) {
         this.value = value;
-        this.unit=unit;
+        this.unit = unit;
     }
 
     @Override
     public boolean equals(Object obj) {
-        if(obj instanceof Length)
-           return this.value==((Length)obj).value;
+        if(this==obj) return true;
+        if (obj instanceof Length)
+            return this.value == ((Length) obj).value;
         return false;
     }
 
--- src/test/java/com/quntity/LengthTest.java
+++ src/test/java/com/quntity/LengthTest.java
@@ -7,34 +7,61 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LengthTest {
 
+    Length withZero = new Length(0, Length.Unit.INCH);
 
     @Test
     void givenZeroFeetAndZeroFeet_whenCompare_TheyShouldBeEqual() {
         Length anotherZeroLength = new Length(0, Length.Unit.FEET);
 
-        final Length zeroFoot = new Length(0,Length.Unit.FEET);
+        final Length zeroFoot = new Length(0, Length.Unit.FEET);
         assertTrue(anotherZeroLength.equals(zeroFoot));
     }
 
     @Test
     void givenZeroFeetAndAAnotherObject_whenCompare_TheyShouldNotBeEqual() {
 
-        assertFalse(new Length(0,Length.Unit.FEET).equals(new Object()));
+        assertFalse(new Length(0, Length.Unit.FEET).equals(new Object()));
     }
 
     @Test
     void givenOneFeetAndTwoFeet_whenCompare_TheyShouldNotBeEqual() {
-        Length oneLength = new Length(1,Length.Unit.FEET);
-        Length twoLength = new Length(2,Length.Unit.FEET);
+        Length oneLength = new Length(1, Length.Unit.FEET);
+        Length twoLength = new Length(2, Length.Unit.FEET);
 
         assertFalse(oneLength.equals(twoLength));
     }
-    @Test
-    void givenZeroFeeAndNull_whenCompare_TheyShouldNotBeEqual(){
 
-        Length zeroLength = new Length(1,Length.Unit.FEET);
+    @Test
+    void givenZeroFeeAndNull_whenCompare_TheyShouldNotBeEqual() {
+
+        Length zeroLength = new Length(1, Length.Unit.FEET);
 
         assertFalse(zeroLength.equals(null));
 
+    }
+
+    @Test
+    void givenZeroInchAndZeroInch_whenCheckEquality_theyShouldBeEqual() {
+        Length withAnotherZero = new Length(0, Length.Unit.INCH);
+
+        assertTrue(withZero.equals(withAnotherZero));
+    }
+
+    @Test
+    void givenZeroInchAndAnotherObject_whenCheckEquality_theyShouldNotBeEqual() {
+
+        assertFalse(withZero.equals(new String()));
+    }
+
+    @Test
+    void givenZeroInchAndOneInch_whenCheckEquality_theyShouldNotBeEqual() {
+
+        assertFalse(withZero.equals(new Length(1, Length.Unit.INCH)));
+    }
+
+    @Test
+    void givenZeroInchAndNull_whenCheckEquality_theyShouldNotBeEqual() {
+
+        assertFalse(withZero.equals(null));
     }
 }
