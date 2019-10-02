9bdda216b9003448f133b209c988dc4fa0b5e8dd
sunilsyenare <syenare97@gmail.com>
2019-10-02 18:13:37 +0530

    Add. Quantity  for remove duplication of equal

--- src/main/java/com/quntity/AddableQuantity.java
+++ src/main/java/com/quntity/AddableQuantity.java
@@ -1,34 +1,13 @@
 package com.quntity;
 
-public class AddableQuantity {
+public class AddableQuantity extends Quantity{
     private final double value;
     private final Unit unit;
 
     protected AddableQuantity(double value, Unit unit) {
+        super(value,unit);
         this.value = value;
         this.unit = unit;
-    }
-
-    private double thisValueInBase() {
-        return (double) Math.round(this.unit.convertToBase(value) * 100) / 100l;
-    }
-
-    private double otherValueInBase(AddableQuantity other) {
-        return (double) Math.round(other.unit.convertToBase(other.value) * 100) / 100;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (!(obj instanceof AddableQuantity))
-            return false;
-        final AddableQuantity other = (AddableQuantity) obj;
-
-        if (!this.unit.getMeasurementType().equals(other.unit.getMeasurementType())) {
-            return false;
-        }
-        return thisValueInBase() == otherValueInBase(other);
     }
 
 
---
+++ src/main/java/com/quntity/Quantity.java
@@ -0,0 +1,34 @@
+package com.quntity;
+
+public class Quantity {
+    private final double value;
+    private final Unit unit;
+
+    protected Quantity(double value, Unit unit) {
+        this.value = value;
+        this.unit = unit;
+    }
+
+    protected double thisValueInBase() {
+        return (double) Math.round(this.unit.convertToBase(value) * 100) / 100;
+    }
+
+    protected double otherValueInBase(Quantity other) {
+        return (double) Math.round(other.unit.convertToBase(other.value) * 100) / 100;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(obj instanceof Quantity))
+            return false;
+        final Quantity other = (Quantity) obj;
+
+        if (!this.unit.getMeasurementType().equals(other.unit.getMeasurementType())) {
+            return false;
+        }
+        return thisValueInBase() == otherValueInBase(other);
+    }
+
+}
